78ab404a72a9f2f0bcbac8e4f2ed3d007b4d6f9b
JoaoFCarvalho <joaofvc946@gmail.com>
2025-05-22 12:42:55 -0300

    Game seed

--- src/main/java/com/game/dslist/entities/Game.java
+++ src/main/java/com/game/dslist/entities/Game.java
@@ -1,7 +1,7 @@
 package com.game.dslist.entities;
 
 import jakarta.persistence.Column;
-import  jakarta.persistence.Entity;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -20,21 +20,27 @@ public class Game {
     @Column(name = "game_year")
     private Integer year;
     private String genre;
-    private String platform;
+    private String platforms;
+    private Double score;
     private String imgUrl;
+
+    @Column(columnDefinition="TEXT")
     private String shortDescription;
+
+    @Column(columnDefinition="TEXT")
     private String longDescription;
 
     public Game(){
 
     }
 
-    public Game (Long id, String title, Integer year, String genre, String platform, String imgUrl, String shortDescription, String longDescription) {
+    public Game (Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl, String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
-        this.platform = platform;
+        this.platforms = platforms;
+        this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
@@ -73,11 +79,19 @@ public class Game {
     }
 
     public String getPlatform() {
-        return platform;
+        return platforms;
     }
 
-    public void setPlatform(String platform) {
-        this.platform = platform;
+    public void setPlatform(String platforms) {
+        this.platforms = platforms;
+    }
+
+        public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
     }
 
     public String getImgUrl() {
@@ -128,5 +142,4 @@ public class Game {
             return false;
         return true;
     }
-    
 }
